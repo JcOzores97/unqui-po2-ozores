b961a739080bf10bad631f4c1c1cd47b8b284251
Juan Cruz Ozores <juanozores97@gmail.com>
2022-04-15 21:30:20 -0300

    incorporación tp4-punto 2

--- src/ar/unq/po2/tp4/ej1/ProductoPrimeraNecesidad.java
+++ src/ar/unq/po2/tp4/ej1/ProductoPrimeraNecesidad.java
@@ -2,17 +2,44 @@ package ar.unq.po2.tp4.ej1;
 
 public class ProductoPrimeraNecesidad extends Producto{
 
+	private int descuentoEnPorcentaje = 10;
+	
 	public ProductoPrimeraNecesidad(String nombre, double precio, Boolean esDePreciosCuidados) {
 		// para invocar al constructor de la super clase uso "super(params)" solamente, si deseo invocar otro metodo es super.xMetodo()
 		 super(nombre, precio, esDePreciosCuidados);
+		
 	}
 	
 	
 	
 
-	@Override
-	public double getPrecio() {
-		return this.precio* 0.9;
+	public ProductoPrimeraNecesidad(String nombre, double precioBase, boolean esDePreciosCuidados, int descuentoEnPorcentaje) {
+		 super(nombre, precioBase, esDePreciosCuidados);
+		 this.setDescuentoEnPorcentaje(descuentoEnPorcentaje);	
 	}
+
+
+
+
+	@Override
+	public double getPrecio() {		
+		double porcentajeSobreTotal = 100 - this.getDescuentoEnPorcentaje();
+		return (porcentajeSobreTotal * this.precio / 100);
+	}
+
+
+
+	public int getDescuentoEnPorcentaje() {
+		return this.descuentoEnPorcentaje;
+	}
+
+
+
+
+	public void setDescuentoEnPorcentaje(int nuevoDescuentoEnPorcentaje) {
+		this.descuentoEnPorcentaje = nuevoDescuentoEnPorcentaje;
+	}
+	
+	
 
 }
--- test/ar/unq/po2/tp4/ej1/ProductoPrimeraNecesidadTest.java
+++ test/ar/unq/po2/tp4/ej1/ProductoPrimeraNecesidadTest.java
@@ -8,10 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ProductoPrimeraNecesidadTest {
 
 	private ProductoPrimeraNecesidad leche;
+	private ProductoPrimeraNecesidad pan;
+	private ProductoPrimeraNecesidad arroz;
 	
 	@BeforeEach
 	public void setUp() {
 		leche = new ProductoPrimeraNecesidad("Leche", 8d, false);
+		pan = new ProductoPrimeraNecesidad("Arroz", 8d, false, 20);
+		arroz = new ProductoPrimeraNecesidad("Pan", 10d, false, 50);
 	}
 	
 	@SuppressWarnings("removal")
@@ -19,4 +23,13 @@ public class ProductoPrimeraNecesidadTest {
 	public void testCalcularPrecio() {
 		assertEquals(new Double(7.2), leche.getPrecio());
 	}
+	
+	@SuppressWarnings("removal")
+	@Test
+	public void testCalculoPrecioConDescuentoVariable(){
+		assertEquals(new Double(6.4), pan.getPrecio());
+		assertEquals(new Double(5), arroz.getPrecio());
+		
+	}
+//	TD: testDescuentoNoMayorA100NiMenorA0;
 }
